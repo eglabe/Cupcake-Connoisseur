6af7374d0dcd6a479b5e4964b756b3fd6f81829e
Erin <eringlabe@gmail.com>
2017-02-04 15:31:58 -0800

    figuring out click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,59 @@
-var wins = 0;
-var losses = 0;
-var userScore = [];
+// A $( document ).ready() block.
+$( document ).ready(function() {
 
-var randomNumber = []
-	for (var i = 19; i <= 120; i++) {
-	randomNumber.push(i) };
-var bakerNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
-console.log(bakerNumber);
+	var wins = 0;
+	var losses = 0;
+	var userScore = new Array;
 
-var cupcakeValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var rainbow = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
-var pink = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
-var red = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
-var chocolate = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
+	var randomNumber = []
+		for (var i = 19; i <= 120; i++) {
+		randomNumber.push(i) };
+	var bakerNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+	console.log(bakerNumber);
 
-console.log(rainbow);
-console.log(pink);
-console.log(red);
-console.log(chocolate);
+	var cupcakeValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	var rainbow = {
+		value: cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)] };
+	var pink = {
+		value: cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)] };
+	var red = {
+		value: cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)] };
+	var chocolate = {
+		value: cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)] };
 
-function reset() {
-	userScore = 0;
-	bakerNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
-	rainbow = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
-	pink = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
-	red = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
-	chocolate = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
-}; 
+	// console.log(rainbow);
+	// console.log(pink);
+	// console.log(red);
+	// console.log(chocolate);
+
+	function reset() {
+		userScore = 0;
+		bakerNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+		rainbow = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
+		pink = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
+		red = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
+		chocolate = cupcakeValues[Math.floor(Math.random() * cupcakeValues.length)];
+	}; 
 
 
-// Set event on clicking a cupcake.
-$( ".cupcake" ).on( "click", function() {
-	console.log( "click!" );
+	// Set event on clicking a cupcake.
+	$("#rainbow").on("click", function() {
+		alert("yay");
+	});
+
+	// Assign cupcake var value to the click.
+
+
+	// When a cupcake is clicked, it's value is added to the user's score.
+
+
+	// When the user's score == the baker's number, the user wins & adds to wins counter.
+
+
+	// If user score > baker's number, the user loses & adds to losses counter.
+
+
+	// If user wins/loses: baker's number and cupcake values regenerate, user score back to 0.
+
 });
-
-// Assign cupcake var value to the click.
-
-
-// When a cupcake is clicked, it's value is added to the user's score.
-
-
-// When the user's score == the baker's number, the user wins & adds to wins counter.
-
-
-// If user score > baker's number, the user loses & adds to losses counter.
-
-
-// If user wins/loses: baker's number and cupcake values regenerate, user score back to 0.
-
 
